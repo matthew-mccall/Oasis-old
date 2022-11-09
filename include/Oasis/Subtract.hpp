50fcc256046c7be7e2accfbe57ea752f698b00d5
Andrew <andrew@thenazareths.net>
2022-11-09 00:53:56 -0500

    Added subtraction; Subtract.cpp, Subtract.hpp, SubtractTest.cpp

---
+++ include/Oasis/Subtract.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Andrew Nazareth on 11/9/22.
+//
+
+#ifndef OASIS_SUBTRACT_HPP
+#define OASIS_SUBTRACT_HPP
+
+#include "Utility/BinaryExpressionNode.hpp"
+
+namespace os {
+
+    class Subtract final : public BinaryExpressionNode {
+    public:
+        Subtract(std::unique_ptr<Expression> &&left, std::unique_ptr<Expression> &&right);
+
+        [[nodiscard]] EvaluateReturnType evaluate() const override;
+
+        OA_EXPRESSION_TYPE(SUBTRACT)
+        OA_DECLARE_FACTORY(Subtract)
+    };
+
+}// namespace os
+
+#endif//OASIS_SUBTRACT_HPP
---
+++ test/SubtractTest.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Andrew Nazareth on 11/9/22.
+//
+#include "spdlog/spdlog.h"
+
+#include "Oasis/Subtract.hpp"
+#include "Oasis/Real.hpp"
+
+void testSubtract(double a, double b) {
+
+    std::unique_ptr<oa::Subtract> subtract = oa::Subtract::Factory {
+        oa::Real::Factory { a },
+        oa::Real::Factory { b }
+    };
+
+    auto [result, error, cause] = subtract->evaluate();
+
+    assert(!error);
+    assert(result->getType() == oa::Expression::Type::REAL);
+
+    auto *realResult = dynamic_cast<oa::Real *>(result.get());
+    assert(realResult->getVal() == (a - b));
+
+    spdlog::info("{} - {} = {}", a, b, realResult->getVal());
+}
+
+int main(int argc, char **argv) {
+
+    testSubtract(2, 3);
+    testSubtract(-1, 1);
+    testSubtract(9, -8);
+
+    testSubtract(3.14, 2.29);
+    testSubtract(-69, 42);
+    testSubtract(6.28, -2);
+}

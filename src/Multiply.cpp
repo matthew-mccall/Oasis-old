31049df72dc78b407f3dbf19e6a90ee30b23877a
Andrew <andrew@thenazareths.net>
2022-11-09 11:18:47 -0500

    Added simple multiplication.

---
+++ src/Multiply.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Andrew Nazareth on 11/9/22.
+//
+
+#include "Oasis/Multiply.hpp"
+#include "Oasis/Real.hpp"
+
+namespace oa {
+    EvaluateReturnType Multiply::evaluate() const {
+        auto [leftResult, rightResult, error, cause] = evaluateOperands();
+
+        if (error) {
+            return { nullptr, error, cause };
+        }
+
+        if (leftResult->getType() == Expression::Type::REAL && rightResult->getType() == Expression::Type::REAL) {
+            Real *leftReal, *rightReal;
+
+            leftReal = dynamic_cast<Real *>(leftResult.get());
+            rightReal = dynamic_cast<Real *>(rightResult.get());
+
+            return EvaluateReturnType { Real::Factory { leftReal->getVal() * rightReal->getVal() } };
+        }
+
+        return EvaluateReturnType { Multiply::Factory { std::move(leftResult), std::move(rightResult) } };
+    }
+
+    Multiply::Multiply(std::unique_ptr<Expression> &&left, std::unique_ptr<Expression> &&right) : BinaryExpressionNode(std::move(left), std::move(right)) { }
+}// namespace oa
